76a8dd73e5f53828b8b8cf78b44855addf6d093d
kirestein <erik.proenca2011@gmail.com>
2022-04-26 23:08:44 -0300

    projeto para o dia 27

--- catalogoDeFilmes/index.js
+++ catalogoDeFilmes/index.js
@@ -1,4 +1,4 @@
-var listaDeFilmes = [
+let listaDeFilmes = [
   "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTphk3rWo-esFX6XPZtJtbmt4JybclqA9I09R5Gjk-DqC8d2_Xb",
   "https://lumiere-a.akamaihd.net/v1/images/690x0w_br_9e5801a5.jpeg?region=0%2C0%2C690%2C1035",
   "https://media.fstatic.com/gzsYUKX6TO32cryrv42xQOsppaE=/290x478/smart/media/movies/covers/2020/05/MASK_OF_ZORRO_1400x2100_EST.jpg",
@@ -7,7 +7,7 @@ var listaDeFilmes = [
   "https://a-static.mlcdn.com.br/1500x1500/quadro-decorativo-o-poderoso-chefao-filmes-cartaz-cinema-decoracoes-com-moldura-g02-vital-quadros/vitalquadrosdobrasil/080008840804/006c5306ba436d3cb06b1468472269dd.jpg"
 ];
 
-var titulos = [
+let titulos = [
   "Shang Chi",
   "Ultimato",
   "Zorro",
@@ -15,19 +15,19 @@ var titulos = [
   "O Impssível",
   "O Poderoso Chefão"
 ];
-var catalogo = document.querySelector("#catalogo");
-var i = 0;
-var erro = document.querySelector("#erro");
+let catalogo = document.querySelector("#catalogo");
 
-for (i; i < listaDeFilmes.length; i++) {
+let erro = document.querySelector("#erro");
+
+for (let i; i < listaDeFilmes.length; i++) {
   catalogo.innerHTML += `<div class="filme"><img src=${listaDeFilmes[i]}><figcaption>${titulos[i]}</figcaption></div>`;
 }
 
-var filmeRepetido = true;
+let filmeRepetido = true;
 
 function Cadastrar() {
-  var novoFilme = document.querySelector("#filme").value;
-  var titulo = document.querySelector("#titulo").value;
+  let novoFilme = document.querySelector("#filme").value;
+  let titulo = document.querySelector("#titulo").value;
 
   if (listaDeFilmes.includes(novoFilme)) {
     erro.innerHTML = `Filme ${novoFilme} já cadastrado.`;
@@ -51,3 +51,5 @@ function mostrarFilmes() {
 function DeleteButton() {
   listaDeFilmes[i].innerHTML = `<button tyoe="submit"> deletar </button>`;
 }
+
+
