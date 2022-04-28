f2173dff35cbc95503cf47274e36f016290aabac
kirestein <erik.proenca2011@gmail.com>
2022-04-28 15:18:16 -0300

    Iniciando a construção do nosso arquivo index.js

---
+++ catDeFilmes/js/index.js
@@ -0,0 +1,17 @@
+let listaDeFilmes = [
+  'https://br.web.img3.acsta.net/pictures/14/08/11/13/34/537036.jpg',
+  'https://img.elo7.com.br/product/original/39B7AE4/quadro-interestelar-a3-com-vidro-quadro-maritimo.jpg',
+  'https://br.web.img3.acsta.net/pictures/210/140/21014024_20130619225537406.jpg',
+  'https://ingresso-a.akamaihd.net/img/cinema/cartaz/6429-cartaz.jpg',
+  'https://upload.wikimedia.org/wikipedia/pt/thumb/4/43/Butterflyeffect_poster.jpg/240px-Butterflyeffect_poster.jpg',
+  'https://upload.wikimedia.org/wikipedia/pt/d/d4/Ready_Player_One_%28filme%29.png'
+]
+
+let listaDeTitulos = [
+  'Relatos Selvagens',
+  'Interestelar',
+  'Truque de mestre',
+  'Cantando na Chuva',
+  'Efeito Borboleta',
+  'Jogador Nº 1'
+]
