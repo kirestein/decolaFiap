e512952bb1ce4f95661532d4e9ebbc1c6b0c32b1
kirestein <erik.proenca2011@gmail.com>
2022-04-28 13:57:50 -0300

    Estudando condicionais

---
+++ catDeFilmes/js/condicionais.js
@@ -0,0 +1,10 @@
+let a = Math.floor(Math.random() * 100)
+console.log(a)
+
+if (a > 50) {
+  console.log('O valor de a está na metade maior da sequência')
+} else if (a === 50) {
+  console.log('O valor de a é igual a 50')
+} else {
+  console.log('O valor de a está na metade menor da sequência')
+}
