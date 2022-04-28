3e62211be7d8968e32314b6471b249438973d12d
kirestein <erik.proenca2011@gmail.com>
2022-04-28 11:06:23 -0300

    estudando funções anônimas e arrow functions

--- catDeFilmes/js/funcoes.js
+++ catDeFilmes/js/funcoes.js
@@ -1,12 +1,36 @@
-let a = Math.floor(Math.random() * 1000)
-function minhaPrimeiraFuncao() {
-  let b = Math.floor(Math.random() * 1000)
-  if(a > b) {
-    return `${a} - ${b} = ${a - b}`
-  } else {
-    return `${a} + ${b} = ${a + b}`
-  }
+// function minhaPrimeiraFuncao(parâmetros) {
+//   let a = Math.floor(Math.random() * 1000)
+//   let b = Math.floor(Math.random() * 1000)
+//   if(a > b) {
+//     return `${a} - ${b} = ${a - b}`
+//   } 
+//   console.log('Estou executando está função!')
   
+  
+// }
+
+// console.log(minhaPrimeiraFuncao())
+
+
+//!esta é uma função anônima
+// const minhaSegundaFuncao = function(parâmetros) {
+//   return 'Escrevendo minha função anônima!'
+// }
+
+// console.log(minhaSegundaFuncao())
+
+//! esta é uma arrow function
+const a = (nome) => {
+  return`Olá ${nome}, esta é uma arrow function`
 }
 
-console.log(minhaPrimeiraFuncao())
+console.log(a('Erik'))
+
+// function b(nome) {
+//   return `Esta é uma arrow function`
+// }
+
+// const c = function(nome) {
+//   return `Esta é uma arrow function`
+// }
+
